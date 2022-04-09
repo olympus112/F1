8ea2426f8c31433f110a2a78f5cc59e07bbce6dc
Matthias <matthivandersanden@gmail.com>
2022-04-09 15:21:07 +0200

    Initial commit

---
+++ js/f1.js
@@ -0,0 +1,77 @@
+const svg_width = 1200;
+const svg_height = 600;
+const margin = {top: 50, right: 50, bottom: 50, left: 50};
+const content_width = svg_width - margin.left - margin.right;
+const content_height = svg_height - margin.top - margin.bottom;
+
+let splitRacerContent = Split(['#racers', '#split-spider-detail'], {
+    sizes: [20, 80],
+    direction: 'vertical',
+});
+
+let splitSpiderDetail = Split(['#split-settings-spider', '#detail'], {
+    sizes: [50, 50],
+    direction: 'horizontal',
+});
+
+let splitSettingsSpider = Split(['#settings', '#spider'], {
+    sizes: [20, 80],
+    direction: 'vertical',
+});
+
+// var svg = d3.select("svg")
+//             .attr("width", svg_width)
+//             .attr("height", svg_height);
+//
+// var content = svg.append("g")
+//                  .attr("class", "content")
+//                  .attr("transform", "translate(" + margin.left + ", " + margin.top + ")")
+//
+// d3.csv("data/results.csv").then(results => {
+//   d3.csv("data/races.csv").then(races => {
+//     let racesPerYear = d3.rollup(
+//       d3.filter(races, d => d.year == "2009"),
+//       v => d3.map(v, d => parseInt(d.raceId)),
+//       d => parseInt(d.year)
+//     );
+//
+//     let currentRaces = racesPerYear[2009];
+//
+//     let meanPointsPerRace = d3.rollup(
+//       d3.filter(results, d => d.raceId in currentRaces),
+//       v => d3.mean(v, d => d.fastestLapTime),
+//       d => parseInt(d.raceId)
+//     );
+//
+//     console.log(meanPointsPerRace);
+//
+//     let xScale = d3.scaleBand()
+//       .domain(meanPointsPerRace.keys())
+//       .range([0, content_width])
+//       .padding(0.2);
+//
+//     let yScale = d3.scaleLinear()
+//       .domain([0, d3.max(meanPointsPerRace.values())])
+//       .range([0, content_height]);
+//
+//     content.append("g")
+//       .attr("class", "axis-bottom")
+//       .attr("transform", "translate(0, " + content_height + ")")
+//       .call(d3.axisBottom(xScale));
+//
+//     content.append("g")
+//       .attr("class", "axis-left")
+//       .call(d3.axisLeft(yScale));
+//
+//     let color = d3.scaleOrdinal(d3.schemeCategory10);
+//
+//     let bars = content.selectAll("rect")
+//       .data(meanPointsPerRace)
+//       .join("rect")
+//       .attr("x", d => xScale(d[0]))
+//       .attr("y", 0)
+//       .attr("width", xScale.bandwidth())
+//       .attr("height", d => yScale(d[1]))
+//       .attr("fill", d => color(d[1]));
+//   });
+// });
---
+++ js/racers.js
@@ -0,0 +1,16 @@
+let div = d3.select("#racers-list");
+
+d3.csv("/data/drivers.csv").then(drivers => {
+    d3.json("/res/images.json").then((images) => {
+        let location = images.location;
+        let racers = images.racers;
+
+        for (let racerId in racers) {
+
+            let image = div.append("img")
+                .attr("width", 50)
+                .attr("height", 50)
+                .attr("src", location + racers[racerId].image);
+        }
+    });
+});
