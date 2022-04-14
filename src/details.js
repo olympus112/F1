dc725fc2229c2ab4e3bc618726e9e394e0853795
Geert Wesemael <onlinegeert@gmail.com>
2022-04-15 01:07:33 +0200

    tryouts

--- src/details.js
+++ src/details.js
@@ -1,6 +1,16 @@
+import { Card, CardHeader, Box } from '@mui/material';
+
+
 export default function Details(props){
+    
     //javascript functies maak je hierboven
+    var s = "Hier komt de details component, dit is de doorgegeven gegevens: " + props.info;
     return(
-        <p>Hier komt de details component, dit is de doorgegeven gegevens: {props.info}</p>
+        <Card>
+            <CardHeader title={s} subheader="ondertitel" />
+            <Box sx={{ p: 3, pb: 1 }} dir="ltr">
+                <p>SVG hier?</p>
+            </Box>
+        </Card>
     )
 }
--- src/spidergraph.js
+++ src/spidergraph.js
@@ -1,6 +1,17 @@
+import { Card, CardHeader, Box } from '@mui/material';
+
 export default function SpiderGraph(props){
     //javascript functies maak je hierboven
     return(
-        <p>Hier komt de spider graph component, dit is de doorgegeven gegevens: {props.info}</p>
+        <Card
+      sx={{
+        py: 5,
+        boxShadow: 0,
+        textAlign: 'center',
+        color: "#05171d",
+        bgcolor: "#e10500",
+      }}>
+<p>Hier komt de spider graph component, dit is de doorgegeven gegevens: {props.info}</p>
+      </Card>
     )
 }
