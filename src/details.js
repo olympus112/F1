3158db2b2e37febe5c8efdd35583729a4940dd41
Thomas Van Erum <37063761+Thomas721@users.noreply.github.com>
2022-04-18 01:21:06 +0200

    Working on race consistency and time consistency

--- src/details.js
+++ src/details.js
@@ -1,10 +1,20 @@
 import { Card, CardHeader, Box } from '@mui/material';
+import * as d3 from 'd3';
+d3.csv("./data/circuits.csv", function(data) {
+    
+    console.log(data[0]);
 
+    for (var i = 0; i < data.length; i++) {
+        // console.log(data[i].circuitId);
+        // console.log(data[i].name);
+    }
+});
 
 export default function Details(props){
     
     //javascript functies maak je hierboven
     var s = "Hier komt de details component, dit is de doorgegeven gegevens: " + props.info;
+
     return(
         <Card>
             <CardHeader title={s} subheader="ondertitel" />
