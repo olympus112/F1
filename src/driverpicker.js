88c916888e72af8a9c4dc5ca6b3c278fd1526ad4
Geert Wesemael <onlinegeert@gmail.com>
2022-04-20 16:13:31 +0200

    driverpicker

--- src/driverpicker.js
+++ src/driverpicker.js
@@ -6,13 +6,14 @@ import * as React from 'react';
 const drivers = ['Max Verstappen','Charles Leclerc','Nico Rosberg']
 
 export default function DriverPicker(props){
-    Object.keys(props.drivers)
+    let options = Object.keys(props.drivers)
     console.log(props.drivers);
     const [value, setValue] = React.useState(props.drivers[1]);
     console.log(props.drivers[1]);
 
-
-
+    if(options.length === 0){
+        return(<div><p>Loading data ...</p></div>)
+    } 
     return(
         <div>
             <Autocomplete
@@ -21,10 +22,10 @@ export default function DriverPicker(props){
                     setValue(newValue); props.influencefunction(newValue)
                 }}
                 id="tags-standard"
-                options={Object.keys(props.drivers)}
+                options={options}
                 // groupBy={(id) => props.drivers[id]}
                 getOptionLabel={(id) => props.drivers[id]}
-                defaultValue={props.drivers[1]}
+                defaultValue={1}
                 renderInput={(params) => (
                 <TextField
                     {...params}
